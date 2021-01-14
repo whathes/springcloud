0f5632cfddbabcc17a22b11408fa21a935ae89e8
wangshuo <1563124347@qq.com>
2021-01-14 16:11:09 +0800

    flyway config

--- server-0/src/main/resources/static/migrate/V1__table_init.sql
+++
@@ -1,5 +0,0 @@
-use springcloud;
-create table if not exists 'init_table'(
-    'id' varchar(32) character utf8 collate utf8_bin not null,
-    'content' varchar(255) not null
-) engine=InnerDB default charset=utf8 collate utf8_bin not null;
