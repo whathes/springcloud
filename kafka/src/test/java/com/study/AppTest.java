04c88851cc8782adddb8aff67a99cf03a059f3a6
wangshuo <1563124347@qq.com>
2021-01-19 14:47:04 +0800

    kafka

--- kafka/src/test/java/com/study/AppTest.java
+++ kafka/src/test/java/com/study/AppTest.java
@@ -3,18 +3,23 @@ package com.study;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringRunner.class)
+@EmbeddedKafka(count = 1,ports = 9093)
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void contextLoads()throws IOException {
+        System.in.read();
     }
+
+
 }
